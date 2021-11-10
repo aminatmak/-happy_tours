e917358a95c054179210e41413c8458d67f2a786
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-11-10 14:39:03 +0400

    search input has been added to the map and bookings are done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,32 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:create, :edit, :update, :destroy]
   def create
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new
+    # Assigning a user to a specific booking
+    @booking.user = current_user
+    # Assigning a tour to a specific booking
+    @booking.tour = Tour.find(params[:tour_id])
+    authorize @booking
+
     if @booking.save
-      redirect_to @booking, notice: 'New booking created!'
+      redirect_to my_dashboard_path, notice: 'New booking created!'
     else
       render :new
     end
   end
 
-  def update
-    @booking.update(booking_params)
-    redirect_to @booking
-  end
+  # def update
+  #   @booking.update(booking_params)
+  #   redirect_to @booking
+  # end
 
-  def destroy
-    @booking.destroy
-    redirect_to root_path
-  end
+  # def destroy
+  #   @booking.destroy
+  #   redirect_to root_path
+  # end
+
+  # private
+
+  # def booking_params
+  #   params.require(:booking).permit(:start_date, :end_date)
+  # end
 end
